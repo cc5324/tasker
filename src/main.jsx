7514043291bbfdb00f6fbb5ea13bcc5002194e15
angela <awander5324@gmail.com>
2023-03-24 10:51:43 +0800

    build(react-dom): setup react-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 
+import ErrorPage from "./layout/share/error-page";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      // loader: rootLoader,
+      // action: rootAction,
+    },
+    // {
+    //   path: "/tasker",
+    //   element: <h1>In takser route.</h1>,
+    // },
+  ],
+  { basename: "/tasker" }
+);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
