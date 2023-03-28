7c29bd5547adbb737fd64976ab23be4f23c8b367
angela <awander5324@gmail.com>
2023-03-28 18:30:08 +0800

    refactor(router): split router as module

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App";
-import ErrorPage from "@/pages/share/error.jsx";
-import LoginPage from "@/pages/login.jsx";
-import Directing, { loader as TokenLoader } from "@/pages/directing.jsx";
-import Tasks from "@/pages/TasksPage.jsx";
-import TaskPage, { loader as TaskLoader } from "@/pages/TaskPage.jsx";
-
-import DemoPage from "@/pages/share/demo.jsx";
+import router, { RouterProvider } from "@/router/router.jsx";
 
 import "./index.css";
-
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      errorElement: <ErrorPage />,
-      // action: rootAction,
-      children: [
-        { path: "login", element: <LoginPage /> },
-        { path: "directing", element: <Directing />, loader: TokenLoader },
-        { path: "tasks", element: <Tasks /> },
-        {
-          path: "task/:taskId",
-          element: <TaskPage />,
-          loader: TaskLoader,
-        },
-      ],
-    },
-    {
-      path: "demo",
-      element: <DemoPage />,
-    },
-  ],
-  { basename: "/tasker" }
-);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
---
+++ src/router/router.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "@/App";
+import ErrorPage from "@/pages/share/error.jsx";
+import LoginPage from "@/pages/login.jsx";
+import Directing, { loader as TokenLoader } from "@/pages/directing.jsx";
+import Tasks from "@/pages/TasksPage.jsx";
+import TaskPage, { loader as TaskLoader } from "@/pages/TaskPage.jsx";
+
+import DemoPage from "@/pages/share/demo.jsx";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      // action: rootAction,
+      children: [
+        { path: "login", element: <LoginPage /> },
+        { path: "directing", element: <Directing />, loader: TokenLoader },
+        { path: "tasks", element: <Tasks /> },
+        {
+          path: "task/:taskId",
+          element: <TaskPage />,
+          loader: TaskLoader,
+        },
+      ],
+    },
+    {
+      path: "demo",
+      element: <DemoPage />,
+    },
+  ],
+  { basename: "/tasker" }
+);
+
+export default router;
+export { RouterProvider };
