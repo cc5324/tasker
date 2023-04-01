a4049aeb2a8f22f12466c00431a61c73549c3919
angela <awander5324@gmail.com>
2023-04-01 18:32:40 +0800

    feat(edit-task): add validation to edit task page

--- src/pages/TaskEditPage.jsx
+++ src/pages/TaskEditPage.jsx
@@ -1,135 +1,69 @@
-import { useState } from "react";
-import {
-  useLoaderData,
-  useNavigate,
-  useSearchParams,
-  Form,
-  redirect,
-} from "react-router-dom";
+import { useLoaderData, useNavigate, useSearchParams } from "react-router-dom";
+import { Formik, Form } from "formik";
+import { object, string } from "yup";
 
 import { GithubAPI } from "@/API";
+import { useTaskStore } from "@/stores/taskStore";
+import { TextField, DropdownField } from "@/component/base";
 
-export default function EditTask() {
+const EditForm = () => {
   const navigate = useNavigate();
+  const stateOptions = useTaskStore((state) => state.stateOptions);
+  const { title, body, labels } = useLoaderData();
+
   let [searchParams, setSearchParams] = useSearchParams();
-  // const { title, body, user, created_at, updated_at, url, id, labels } =
-  //   useLoaderData();
-  const task = useLoaderData();
-  // console.log(`task`, task);
-
-  const currentState = task.labels[0]?.name ?? "Open";
-
-  const [state, setState] = useState(currentState);
-  const [title, setTitle] = useState(task.title);
-  const [body, setBody] = useState(task.body);
-
-  function handleClick(event, value) {
-    event.stopPropagation();
-    const elem = document.activeElement;
-    elem?.blur();
-    setState(value);
-  }
-
-  async function handleSubmit(event) {
-    console.log(`handleSubmit`, event);
-    event.preventDefault();
-    const form = new FormData(event.target);
-
-    const { title, body, state } = Object.fromEntries(form.entries());
-
-    const { owner, repo, number } = Object.fromEntries(searchParams.entries());
-
-    try {
-      const response = await GithubAPI.PATCH(
-        `/repos/${owner}/${repo}/issues/${number}`,
-        { title, body, labels: [state] }
-      );
-      console.log(`res`, response);
-      // redirect(`/task/:${task.id}`, { replace: true });
-      navigate(
-        `/task/:${task.id}?owner=${owner}&repo=${repo}&number=${number}`,
-        { replace: true }
-      );
-    } catch (error) {
-      console.log(error);
-      // redirect("/tasks", { replace: true });
-      navigate("/tasks", { replace: true });
-    }
-  }
+  const { owner, repo, number } = Object.fromEntries(searchParams.entries());
 
   return (
-    <div className=" bg-white p-8">
-      <form method="post" onSubmit={handleSubmit}>
-        <div className="dropdown dropdown-right">
-          <label tabIndex={0} className="btn m-1 rounded-lg">
-            {state}
-            <input
-              type="text"
-              placeholder="Type here"
-              className="input input-ghost w-full max-w-xs hidden"
-              name="state"
-              value={state}
-              onChange={(e) => setState(e.target.value)}
-            />
-          </label>
-          <ul
-            tabIndex={0}
-            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li onClick={(event) => handleClick(event, "Open")}>
-              <a>Open</a>
-            </li>
-            <li onClick={(event) => handleClick(event, "In Progress")}>
-              <a>In Progress</a>
-            </li>
-            <li onClick={(event) => handleClick(event, "Done")}>
-              <a>Done</a>
-            </li>
-          </ul>
-        </div>
-        <div className="flex items-center mb-3">
-          <div className="form-control w-full">
-            <label className="label">
-              <span className="label-text">Title</span>
-            </label>
-            <input
-              type="text"
-              placeholder="Type here"
-              className="input input-bordered w-full"
+    <>
+      <Formik
+        initialValues={{
+          title: title,
+          body: body,
+          state: labels[0]?.name ?? "Open",
+        }}
+        validationSchema={object({
+          title: string().max(30, "不得多於 30 個字").required("必填"),
+          body: string().min(10, "不得少於 30 個字").required("必填"),
+          state: string()
+            .oneOf(["open", "in progress", "done"])
+            .required("Required"),
+        })}
+        onSubmit={async ({ title, body, state }) => {
+          try {
+            const response = await GithubAPI.PATCH(
+              `/repos/${owner}/${repo}/issues/${number}`,
+              { title, body, labels: [state] }
+            );
+            console.log(`res`, response);
+            navigate(
+              `/task/:${task.id}?owner=${owner}&repo=${repo}&number=${number}`,
+              { replace: true }
+            );
+          } catch (error) {
+            console.log(error);
+            navigate("/tasks", { replace: true });
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="p-8">
+            <DropdownField name="state" options={stateOptions} />
+            <TextField
+              label="Title"
               name="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
+              type="text"
+              placeholder="Title"
             />
-            <label className="label">
-              <span className="label-text-alt">Error hint</span>
-            </label>
-          </div>
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Your bio</span>
-          </label>
-          <textarea
-            className="textarea textarea-bordered"
-            placeholder="Bio"
-            rows="7"
-            name="body"
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-          ></textarea>
-          <label className="label">
-            <span className="label-text-alt">Error hint</span>
-          </label>
-        </div>
-        <div>
-          <button className="btn btn-info mr-2" type="submit">
-            Save
-          </button>
-          <button className="btn btn-error" onClick={() => navigate("/tasks")}>
-            Cancel
-          </button>
-        </div>
-      </form>
-    </div>
+            <TextField label="body" name="body" type="textarea" rows={7} />
+            <button type="submit" className="btn" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
-}
+};
+
+export default EditForm;
